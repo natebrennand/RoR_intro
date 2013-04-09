014d74e69947c2a3149c7ea353a004186e7802cd
Nate Brennnand <natebrennand@gmail.com>
2013-04-09 00:26:17 -0400

    --no-active-record != --skip-active-record
    no errors were thrown.....

--- lab_2/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Lab2::Application.config.secret_token = 'e864bd6a1b6b7f24586dab410d9b92f08d8e5bf4eca83583fc19b4914c16938dd7c9dc6660f2fcb4281f35ed4772cb7f632ff839b3a3bfd2681b2d58d0405b7c'
