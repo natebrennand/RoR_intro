627e9074bd4501aad7b6242ba02aee95ce23ce2b
Nate Brennnand <natebrennand@gmail.com>
2013-04-18 15:52:57 -0400

    New Course fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,13 @@
 class Course
   include Mongoid::Document
-  field :name, type: String
-  field :professor, type: String
+  field :call_number
+  field :course_title
+  field :start_time, type: Date
+  field :end_time, type: Date
+  field :days           # Day of the week it meets
+  field :building
+  field :room, type: Integer
+  field :professor
   has_and_belongs_to_many :students
   has_many :lectures
 end
