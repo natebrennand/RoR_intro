59b94e7e58511826d4396bfe5c59f5d8ffb1ef5f
Nate Brennnand <natebrennand@gmail.com>
2013-04-18 17:07:09 -0400

    Update course fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,13 @@
 class Course
   include Mongoid::Document
-  field :name, type: String
-  field :professor, type: String
+  field :call_number, type: Integer
+  field :course_title
+  field :start_time
+  field :end_time
+  field :days           # Day of the week it meets
+  field :building
+  field :room, type: Integer
+  field :professor
   has_and_belongs_to_many :students
   has_many :lectures
 end
