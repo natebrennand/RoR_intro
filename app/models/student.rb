bc92b7b82b67e5fe8ebd0d31eb7f9c86068328f8
Nate Brennnand <natebrennand@gmail.com>
2013-05-10 12:56:14 -0400

    Added role field to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,7 @@ class Student
   field :name
   field :uni
   field :email
+  field :role, type: Boolean     # False : student, True : admin
   has_and_belongs_to_many :courses
 
   validates_uniqueness_of :uni
